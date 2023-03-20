320c33927c15af1f4bfd05e55279ef1a61326e1e
Atryom-Bohaturou <81459880+Atryom-Bohaturou@users.noreply.github.com>
2023-03-20 18:52:30 +0300

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,7 +10,7 @@ class AdminController extends Controller
 {
     public function index()
     {
-        $data = Applications::all();
+        $data = Applications::with('user')->get();
 
         return view('admin', compact('data'));
     }
